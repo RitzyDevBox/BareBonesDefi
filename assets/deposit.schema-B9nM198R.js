4d91adb59cd65bbedce706a8312886d428d55414
RitzyDev <RitzyDevBox@proton.me>
2026-04-18 20:59:32 -0400

    Deploy from main at 2026-04-19T00:59:21Z

--- assets/deposit.schema-B9nM198R.js
+++
@@ -1,1 +0,0 @@
-import{U as e,F as o}from"./index-DoioOLSO.js";const t={fields:[{id:"asset",component:o.TOKEN_AMOUNT_PICKER,label:"Asset",options:{userScope:e.Account}}]};export{t as DEPOSIT_SCHEMA,t as default};
---
+++ assets/deposit.schema-Beu0iEOU.js
@@ -0,0 +1,1 @@
+import{U as e,F as o}from"./index-98DHHn2O.js";const t={fields:[{id:"asset",component:o.TOKEN_AMOUNT_PICKER,label:"Asset",options:{userScope:e.Account}}]};export{t as DEPOSIT_SCHEMA,t as default};
