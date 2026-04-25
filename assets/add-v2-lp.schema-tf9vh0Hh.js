5684696fc45dc396cd1895f85adacad017860abe
RitzyDev <RitzyDevBox@proton.me>
2026-04-24 20:37:46 -0400

    gh-pages live deploy from modern-designs @ 2026-04-25T00:37:36Z

---
+++ assets/add-v2-lp.schema-tf9vh0Hh.js
@@ -0,0 +1,1 @@
+import{F as o}from"./index-Bwh9Ejuy.js";const e={fields:[{id:"tokenA",component:o.TOKEN_AMOUNT_PICKER,label:"Token A"},{id:"amountA",component:o.AMOUNT,label:"Amount A"},{id:"tokenB",component:o.TOKEN_AMOUNT_PICKER,label:"Token B"},{id:"amountB",component:o.AMOUNT,label:"Amount B"}]};export{e as ADD_V2_LP_SCHEMA,e as default};
--- assets/remove-v2-lp.schema-C6qHubTv.js
+++
@@ -1,1 +0,0 @@
-import{F as o}from"./index-BW-qAzOO.js";const e={fields:[{id:"lpAmount",component:o.AMOUNT,label:"LP Amount"}]};export{e as REMOVE_V2_LP_SCHEMA,e as default};
